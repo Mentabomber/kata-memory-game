20439204a753da702bb347bd8ab01cc04fd6b0e0
Mentabomber <mentae_mentology@libero.it>
2024-07-15 13:39:00 +0200

    start

---
+++ JS/main.js
@@ -0,0 +1,32 @@
+const memoryArray = [
+  {
+    id: 1,
+    name: "alien",
+    img: "../images/alien.png",
+  },
+  {
+    id: 2,
+    name: "bug",
+    img: "../images/bug.png",
+  },
+  {
+    id: 3,
+    name: "duck",
+    img: "../images/duck.png",
+  },
+  {
+    id: 4,
+    name: "rocket",
+    img: "../images/rocket.png",
+  },
+  {
+    id: 5,
+    name: "spaceship",
+    img: "../images/spaceship.png",
+  },
+  {
+    id: 6,
+    name: "tiktac",
+    img: "../images/tiktac.png",
+  },
+];
