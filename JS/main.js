d9c4e6609d720dc0ca0b7f3193214077af857356
Mentabomber <mentae_mentology@libero.it>
2024-07-21 12:37:34 +0200

    js final update

--- JS/main.js
+++ JS/main.js
@@ -1,184 +1,96 @@
-// ARRAY OF OBJECTS CONTAINING MEMORY CARDS WITH ID - NAME - IMG
-const memoryArray = [
-  {
-    id: 1,
-    name: "alien",
-    img: "../images/alien.png",
-  },
-  {
-    id: 2,
-    name: "bug",
-    img: "../images/bug.png",
-  },
-  {
-    id: 3,
-    name: "duck",
-    img: "../images/duck.png",
-  },
-  {
-    id: 4,
-    name: "rocket",
-    img: "../images/rocket.png",
-  },
-  {
-    id: 5,
-    name: "spaceship",
-    img: "../images/spaceship.png",
-  },
-  {
-    id: 6,
-    name: "tiktac",
-    img: "../images/tiktac.png",
-  },
-];
 // MEMORY RULES AND OBJECTIVE
 // User can click any of the blank memory card boxes, when that happens the card selected will be revealed and won't be interactable anymore until another card will be interacted with. IF the next card revealed is of the same TYPE (ID) then both cards will become not interactable until page is reloaded or reset button is pressed ELSE both cards will become blank again. WHEN all the copies have been discovered game ends, everytime a copy is not found a counter with an error number will be updated.
 
-// funzione
-// crea 12 box
-// box devi aver div dentro con un elemento preso a random dall'array de oggetti che go crea sorae non pol aver uno de quei elementi se ge ne se stai za creadi due de quel tipo el div inoltre devi aver la classe blank che no me fa veder che tipo se fin che no lo clicko
+const memoryArray = [
+  { id: 1, name: "alien", img: "../images/alien.png" },
+  { id: 2, name: "bug", img: "../images/bug.png" },
+  { id: 3, name: "duck", img: "../images/duck.png" },
+  { id: 4, name: "rocket", img: "../images/rocket.png" },
+  { id: 5, name: "spaceship", img: "../images/spaceship.png" },
+  { id: 6, name: "tiktac", img: "../images/tiktac.png" },
+];
 
 let gridElement = document.getElementById("memory-cards-container");
-
-// ARRAY that takes care of the cards that are being generated and prevents the repetition of a type more than twice
-const cardsArray = [];
-
-let index, errorCounter;
-
-// const playButton = document.getElementById("button-play");
+let cardsArray = [];
+let selectedCards = [];
+let errorCounter = 0;
 
 function idNumberGenerator() {
-  const minCeiled = Math.ceil(1);
-  const maxFloored = Math.floor(memoryArray.length + 1);
-  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+  return Math.floor(Math.random() * memoryArray.length);
 }
-// Function that counts how many times any value is found in an array
-function countInArray(array, what) {
-  let count = 0;
-  for (let index = 0; index < array.length; index++) {
-    if (array[index] === what) {
-      count++;
+
+function shuffle(array) {
+  return array.sort(() => Math.random() - 0.5);
+}
+
+function cardsArrayPopulator() {
+  cardsArray = [];
+  while (cardsArray.length < 12) {
+    let idNumber = idNumberGenerator();
+    let card = memoryArray[idNumber];
+    if (cardsArray.filter((c) => c.id === card.id).length < 2) {
+      cardsArray.push(card);
     }
   }
-  return count;
+  cardsArray = shuffle(cardsArray);
 }
-// Function that populates the array of cards
-function cardsArrayPopulator() {
-  let idNumber;
-  while (cardsArray.length < 12) {
-    idNumber = idNumberGenerator();
-    // IF cardsArray has 2 or more elements of the same type generate another card number to push inside the array
-    if (!(countInArray(cardsArray, idNumber) >= 2)) {
-      cardsArray.push(idNumber);
+
+function cardClickHandler(event) {
+  const cardElement = event.target;
+  if (cardElement.classList.contains("revealed") || selectedCards.length === 2)
+    return;
+  cardElement.classList.remove("blank");
+  cardElement.classList.add("revealed");
+  cardElement.style.backgroundImage = `url(${cardElement.getAttribute(
+    "data-img"
+  )})`;
+  selectedCards.push(cardElement);
+
+  if (selectedCards.length === 2) {
+    const [firstCard, secondCard] = selectedCards;
+    const firstCardId = firstCard.getAttribute("data-id");
+    const secondCardId = secondCard.getAttribute("data-id");
+
+    if (firstCardId === secondCardId) {
+      selectedCards = [];
+    } else {
+      setTimeout(() => {
+        firstCard.classList.add("blank");
+        firstCard.classList.remove("revealed");
+        firstCard.style.backgroundImage = 'url("../images/back.png")';
+        secondCard.classList.add("blank");
+        secondCard.classList.remove("revealed");
+        secondCard.style.backgroundImage = 'url("../images/back.png")';
+        selectedCards = [];
+        errorCounter++;
+        document.getElementById(
+          "error-counter"
+        ).textContent = `Errors: ${errorCounter}`;
+      }, 1000);
     }
   }
 }
 
 function cardCreation() {
   cardsArrayPopulator();
-
   gridElement.replaceChildren();
-
-  for (let i = 0; i < cardsArray.length; i++) {
-    gridElement.append(cardsArray[i]);
-  }
+  cardsArray.forEach((card) => {
+    const cardElement = document.createElement("div");
+    cardElement.classList.add("card", "blank");
+    cardElement.setAttribute("data-id", card.id);
+    cardElement.setAttribute("data-img", card.img);
+    cardElement.style.backgroundImage = 'url("../images/back.png")';
+    cardElement.addEventListener("click", cardClickHandler);
+    gridElement.appendChild(cardElement);
+  });
 }
 
+document.getElementById("reset-button").addEventListener("click", () => {
+  errorCounter = 0;
+  document.getElementById(
+    "error-counter"
+  ).textContent = `Errors: ${errorCounter}`;
+  cardCreation();
+});
+
 cardCreation();
-
-// function boxCreation(elementType, className) {
-//   const newElement = document.createElement(elementType);
-
-//   newElement.classList.add(className);
-
-//   return newElement;
-// }
-
-// const playButton = document.getElementById("button-play");
-
-// playButton.addEventListener("click", function () {
-//   gridElement.replaceChildren();
-
-//   const boxes = 12;
-
-//   for (let i = 0; i < boxes.length; i++) {
-//     gridElement.append(boxes[i]);
-//   }
-// });
-
-// function createGrid(boxNum, gridStyle) {
-//   const boxes = [];
-
-//   let score = 0;
-
-//   let listBombs = createArray(16);
-
-//   console.log(listBombs);
-
-//   for (let i = 0; i < boxNum; i++) {
-//     let newBox = boxCreation("div", gridStyle);
-
-//     const newSpan = document.createElement("span");
-
-//     newBox.append(newSpan);
-//     // console.log(createArray(index));
-
-//     newSpan.append(i + 1);
-
-//     console.log(parseInt(newSpan.innerHTML));
-
-//     let actualScore = document.getElementById("score");
-
-//     newBox.addEventListener(
-//       "click",
-
-//       function () {
-//         console.log(boxNum);
-//         console.log(parseInt(newSpan.innerHTML));
-
-//         let controlloBox = document.querySelectorAll(
-//           "div.easy-grid-box span, div.hard-grid-box span, div.very-hard-grid-box span"
-//         );
-//         console.log(controlloBox);
-
-//         // for(let i = 0; i < boxNum; i++){
-//         if (listBombs.includes(parseInt(newSpan.innerHTML))) {
-//           newBox.classList.add("bomb-box");
-//           for (let i = 0; i < boxNum; i++) {
-//             // if (parseInt(controlloBox[i].innerHTML) == parseInt(listBombs[index])){
-//             //     newBox.classList.add("bomb-box");
-//             //     index++;
-//             //     i = 0;
-//             // }
-//             if (listBombs.includes(i + 1)) {
-//               controlloBox[i].parentElement.classList.add("bomb-box");
-//               console.log(listBombs);
-//             }
-//             // console.log(i);
-//             // console.log(index);
-//           }
-
-//           alert("hai perso! il tuo score è: " + score);
-//           score = 0;
-//           // gridElement.innerHTML = "";
-//         } else if (score == boxes.length - listBombs.length - 1) {
-//           score = score + 1;
-//           alert("hai vinto complimenti!");
-//           score = 0;
-//           gridElement.innerHTML = "";
-//         } else {
-//           newBox.classList.add("flower-box");
-//           score = score + 1;
-//           // console.log(score);
-//         }
-
-//         actualScore.innerHTML = `il tuo score attuale è :${score}`;
-
-//         // console.log(i + 1);
-//       }
-//       // }
-//     );
-//     boxes.push(newBox);
-//   }
-//   return boxes;
-// }
